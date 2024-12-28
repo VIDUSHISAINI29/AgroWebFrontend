8434e839ffb6ba1b906de1c98b29c263861a362a
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2024-12-28 21:01:05 +0530

    agro based full stack project beginning

---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
+
+<template>
+   <div>
+      <div class="wineGlassTable tw-h-[80vh]"></div>
+   </div>
+</template>
+
+<style scoped>
+.wineGlassTable{
+   background-image: url('/images/greenLeaves.jpg');
+   background-size: cover;
+   background-repeat: no-repeat;
+
+}
+</style>
