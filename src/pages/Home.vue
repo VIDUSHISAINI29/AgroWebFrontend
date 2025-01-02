f758ae3a3b275451a8c84377ed69acbeb4b11359
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2025-01-02 14:48:26 +0530

    Api of every crop is created

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -21,10 +21,11 @@ onMounted(() => {
  
 <template>
    <div>
-      <div class="wineGlassTable tw-flex tw-justify-center tw-items-center tw-h-[80vh]">
+      <div class="wineGlassTable tw-flex tw-justify-end tw-items-center tw-h-[80vh]">
         <div>
-         <span class="text  tw-block tw-w-[600px] tw-text-7xl">
-            Growing prosperity nurturing nature.
+          <div class="navbar"></div>
+         <span class="text  tw-block tw-w-[570px] tw-m-3 tw-text-[4rem]">
+          Fields whisper stories of resilience, growth, and hope.
          </span>
         </div>
       </div>
@@ -33,35 +34,18 @@ onMounted(() => {
 
 <style scoped>
 .wineGlassTable{
-   background-image: url('/images/greenLeaves.jpg');
+   background-image: url('/images/field.jpg');
    background-size: cover;
    background-repeat: no-repeat;
 
 }
 .text{
-   -webkit-text-stroke: 1px white; 
+   -webkit-text-stroke: 1px rgb(67, 144, 0); 
    color: transparent;
    animation: outline-animation 3s ease-in-out;
    transition: all;
+   line-height: 100%;
 }
-@keyframes outline-animation {
-      0% {
-        -webkit-text-stroke: 0px transparent; /* Start with no outline */
-      }
-      100% {
-        -webkit-text-stroke: 1px rgb(193, 186, 186); /* End with a solid black outline */
-      }
-    }
-    .outlined-text:nth-child(1) {
-      animation-delay: 0s; /* First word starts immediately */
-    }
 
-    .outlined-text:nth-child(2) {
-      animation-delay: 1s; /* Second word starts after 0.5s */
-    }
-
-    .outlined-text:nth-child(3) {
-      animation-delay: 2s; /* Third word starts after 1s */
-    }
 
 </style> 
